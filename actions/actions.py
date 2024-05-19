83afd0daec5e09dd222a68452845688607a51d79
anupshrestha886 <anupshrestha886@gmail.com>
2024-05-19 18:24:22 +0545

    Solved all warnings and issue

--- actions/actions.py
+++ actions/actions.py
@@ -5,14 +5,14 @@
 # https://rasa.com/docs/rasa/custom-actions
 
 
-# This is a simple example for a custom action which utters "Hello World!"
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
+
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+
+
 # class ActionHelloWorld(Action):
 #
 #     def name(self) -> Text:
@@ -26,37 +26,88 @@
 #
 #         return []
 
-from rasa.core.actions.action import Action
-from rasa import core
 
-class AddMedicationsAction(Action):
-    
-    def run(self, dispatcher, tracker):
-        medication_name = tracker.get_slot("medication_name")
-        response = f"API called to add medication: {medication_name}"
-        dispatcher.utter_message(response)
-        return core.ActionExecutionResult.success()
-    
-class ListMedicationsAction(Action):
-    
-    def run(self, dispatcher, tracker):
-        response = f"API called to list the medications names"
-        dispatcher.utter_message(response)
-        return core.ActionExecutionResult.success()
-    
-class AboutDiseasesAction(Action):
-    
-    def run(self, dispatcher, tracker):
+# from rasa import core
+# from rasa.core.actions.action import Action
+#import openai
 
-        disease_name = tracker.get_slot("disease_name")
-        response = f"API called to retrieve information about {disease_name}"
-        dispatcher.utter_message(response)
-        return core.ActionExecutionResult.success()
 
-class NotifyMedicationTakenAction(Action):
+class actionaddmedication(Action):
+    def name(self) -> Text: 
+        return "action_add_medication"
     
-    def run(self, dispatcher, tracker):
-        medication_name = tracker.get_slot("medication_name")
-        response = f"API called to record that {medication_name} is taken."
-        dispatcher.utter_message(response)
-        return core.ActionExecutionResult.success()
+    def run(self, dispatcher: CollectingDispatcher, 
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        
+        medication = tracker.get_slot("add_medication")
+        
+        if not medication:
+            response = "sorry! i did not get medication name"
+        else:
+            response = f"API called to add medication: {medication}"
+
+        dispatcher.utter_message(text = response)
+        
+        return []
+    
+class actionmedicationname(Action):
+    
+    def name(self) -> Text:
+        return "action_medication_name"
+    
+    def run(self, dispatcher: CollectingDispatcher, 
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        
+        response = "API called to list medication name"
+
+        dispatcher.utter_message(text = response)
+        
+        return []
+    
+class actiondiseasedetails(Action):
+    
+    def name(self) -> Text:
+        return "action_disease_details"
+    
+    def run(self, dispatcher: CollectingDispatcher, 
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        
+        disease = tracker.get_slot("about_diseases")
+        
+        response = f"API called to get {disease} details"
+
+        dispatcher.utter_message(text = response)
+        
+        return []
+
+
+# class OpenAIRequestAction(Action):
+
+#     def __init__(self):
+#         API_KEY = "sk-proj-bpuOX2JgxmLeYuZ1vMPrT3BlbkFJZZT8sUEApTIBsCHmGdN6"
+#         openai.api_key = "API_KEY"
+
+#     def run(self, dispatcher, tracker):
+
+#         user_query = tracker.get_slot("user_query")
+        
+#         prompt = f"Here is a medical query from a user: {user_query}"
+        
+#         response = openai.Completion.create(
+#             model = "gpt-4-turbo"
+#             engine="text-davinci-003",
+#             prompt=prompt,
+#             max_tokens=1024,
+#             n=1,
+#             stop=None,
+#             temperature=0.7,
+#         )
+        
+#         openai_response = response.choices[0].text.strip()
+       
+#         dispatcher.utter_message(openai_response)
+        
+#         return core.ActionExecutionResult.success()
